7fbce0455f332e737e1635a7386e8d1dc0f85858
clickys <koc.clicky@gmail.com>
2019-03-20 23:39:33 +0200

    refactor the whole exercise

--- app.js
+++ app.js
@@ -10,60 +10,34 @@ const app = {
             .toString( 36 )
             .substr( 2, 9 ) }`;
     },
-};
 
-const view = {
-    render() {
-        const ulElement = document.querySelector( '.list-notes' );
-        const userInput = document.querySelector( '.add-todo' );
-        ulElement.innerHTML = '';
-        for ( let i = 0; i < app.notes.length; i++ ) {
-            const liElement = document.createElement( 'li' );
-            liElement.dataset.id = app.notes[ i ].id;
-            liElement.innerHTML = '<i class="far fa-dot-circle dropdown-button fa-2x expand-btn"></i> ';
-            liElement.innerHTML += app.notes[ i ].userInput;
-            liElement.innerHTML += '<i class="fas fa-edit edit-btn fa-2x"></i>';
-            liElement.innerHTML += '<i class="fas fa-trash delete-btn fa-2x"></i>';
-            ulElement.appendChild( liElement );
-        }
-        userInput.value = '';
-        userInput.focus();
-    },
-};
-const controller = {
-    addTodo( e ) {
-        const userInput = document.querySelector( '.add-todo' ).value;
-        if ( e.keyCode === 13 ) {
-            if ( userInput.trim() === '' ) {
-                return;
-            }
-            app.notes.push( {
-                userInput,
-                completed: false,
-                id: app.generateUniqueID(),
-            } );
-            view.render();
-        }
-    },
-
-    editTodo( e ) {
-        const noteID = e.target.parentElement.dataset.id;
-        const a = app.notes.filter( val => val.id === noteID );
-        document.querySelector( '#editInput' ).style.display = 'block';
-    },
-
-    update() {
-        console.log( 'update' );
-    },
-    setupEventListeners() {
-        document.querySelector( '.add-todo' ).addEventListener( 'keyup', this.addTodo.bind( this ) );
-        document.querySelector( '#editInput' ).addEventListener( 'focusout', this.update.bind( this ) );
-        document.querySelector( 'ul' ).addEventListener( 'dblclick', ( e ) => {
-            if ( e.target.closest( '.edit-btn' ) ) {
-                this.editTodo( e );
-            }
+    addNote( whereToAddNote, userNote ) {
+        whereToAddNote.push( {
+            userNote,
+            id: this.generateUniqueID(),
+            nestedNotes: [],
         } );
     },
-};
 
-controller.setupEventListeners();
+    editNote( arrayToCheck, noteIdToEdit, newNote ) {
+        for ( let i = 0; i < arrayToCheck.length; i++ ) {
+            if ( arrayToCheck[ i ].nestedNotes.length ) {
+                this.editNote( arrayToCheck[ i ].nestedNotes, noteIdToEdit, newNote );
+            } else if ( arrayToCheck[ i ].id === noteIdToEdit ) {
+                arrayToCheck[ i ].userNote = newNote;
+                break;
+            }
+        }
+    },
+
+    deleteNote( arrayToCheck, noteIdToEdit ) {
+        for ( let i = 0; i < arrayToCheck.length; i++ ) {
+            if ( arrayToCheck[ i ].nestedNotes.length ) {
+                this.deleteNote( arrayToCheck[ i ].nestedNotes, noteIdToEdit );
+            } else if ( arrayToCheck[ i ].id === noteIdToEdit ) {
+                arrayToCheck.splice( i, 1 );
+                break;
+            }
+        }
+    },
+};
